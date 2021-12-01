c5b13d1b9df2c74bb56a6b04e08e23c3d1fa2027
KRiedmiller <kai.riedmiller@web.de>
2021-12-01 17:59:08 +0100

    change cwd to out_dir

--- src/kimmdy/mdmanager.py
+++ src/kimmdy/mdmanager.py
@@ -37,9 +37,9 @@ class MDManager:
         tpr = out_dir / "minimization.tpr"
         outgro = out_dir / "minimized.gro"
         run_shell_cmd(
-            f"gmx grompp -p {top} -c {gro} -r {gro} -f {mdp} -o {tpr}"
+            f"gmx grompp -p {top} -c {gro} -r {gro} -f {mdp} -o {tpr}", out_dir
         )
-        run_shell_cmd(f"gmx mdrun -s {tpr} -c {outgro}")
+        run_shell_cmd(f"gmx mdrun -s {tpr} -c {outgro}", out_dir)
     
     def _minimzation(self):
 
@@ -52,9 +52,9 @@ class MDManager:
         tpr = out_dir / "equil.tpr"
         outgro = out_dir / "equil.gro"
         run_shell_cmd(
-            f"gmx grompp -p {top} -c {gro} -r {gro} -f {mdp} -o {tpr}"
+            f"gmx grompp -p {top} -c {gro} -r {gro} -f {mdp} -o {tpr}", out_dir
         )
-        run_shell_cmd(f"gmx mdrun -v -s {tpr} -c {outgro}")
+        run_shell_cmd(f"gmx mdrun -v -s {tpr} -c {outgro}", out_dir)
 
     def equilibrium(self, out_dir, top, gro, mdp, idx):
         """equilibrium before pulling md"""
@@ -62,10 +62,10 @@ class MDManager:
         outgro = out_dir / "equil.gro"
         outtrr = out_dir / "equil.trr"
         run_shell_cmd(
-            f"gmx grompp -p {top} -c {gro} -f {mdp} -n {idx} -o {tpr} -maxwarn 5"
+            f"gmx grompp -p {top} -c {gro} -f {mdp} -n {idx} -o {tpr} -maxwarn 5", out_dir
         )
         run_shell_cmd(
-            f"gmx mdrun -v -s {tpr} -c {outgro} -o {outtrr}"
+            f"gmx mdrun -v -s {tpr} -c {outgro} -o {outtrr}", out_dir
         )  # use mpirun mdrun_mpi on cluster / several nodes are used
 
     def production(self, out_dir, top, gro, mdp, idx, cpt, dat):
@@ -74,10 +74,10 @@ class MDManager:
         outgro = out_dir / "prod.gro"
         outtrr = out_dir / "prod.trr"
         run_shell_cmd(
-            f" gmx grompp -p {top} -c {gro} -f {mdp} -n {idx} -t {cpt} -o {tpr} -maxwarn 5"
+            f" gmx grompp -p {top} -c {gro} -f {mdp} -n {idx} -t {cpt} -o {tpr} -maxwarn 5", out_dir
         )
         run_shell_cmd(
-            f"gmx mdrun -v -s {tpr} -c {outgro} -plumed {dat} -o {outtrr}"
+            f"gmx mdrun -v -s {tpr} -c {outgro} -plumed {dat} -o {outtrr}", out_dir
         )  # use mpirun mdrun_mpi on cluster
     
     def _production(self, cpt, plumedfile):
@@ -96,10 +96,10 @@ class MDManager:
         outgro = out_dir / "relax.gro"
         outtrr = out_dir / "relax.trr"
         run_shell_cmd(
-            f" gmx grompp -p {top} -c {gro} -f {mdp} -n {idx} -t {cpt} -o {tpr} -maxwarn 5"
+            f" gmx grompp -p {top} -c {gro} -f {mdp} -n {idx} -t {cpt} -o {tpr} -maxwarn 5", out_dir
         )
         run_shell_cmd(
-            f"gmx mdrun -v -s {tpr} -c {outgro} -o {outtrr}"
+            f"gmx mdrun -v -s {tpr} -c {outgro} -o {outtrr}", out_dir
         )  # use mpirun mdrun_mpi on cluster / several nodes are used
 
     def _relaxation(self, cpt):
