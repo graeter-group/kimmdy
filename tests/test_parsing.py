52e0fa71231c1279ea8b18ec1f39e1ce3de2e571
Jannik Buhr <jannik.m.buhr@gmail.com>
2021-12-10 16:05:07 +0100

    refine topology parsing

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -7,7 +7,7 @@ allowed_text = st.text(string.ascii_letters +
                        string.digits +
                        '!"$%&\'()*+,-./:<=>?@\\^_`{|}~', min_size=1)
 
-@given(d=st.dictionaries(allowed_text, st.lists(allowed_text, min_size=1)))
+@given(d=st.dictionaries(allowed_text, st.lists(st.lists(allowed_text, min_size=1), min_size=1)))
 def test_parser_invertible(d):
     p = Path("/tmp/pytest_topol.top")
     parsing.write_topol(d, p)
