aa8832713d2f48bde42091a8e00b7fe532d34961
Jannik Buhr <jannik.m.buhr@gmail.com>
2021-12-15 14:50:22 +0100

    Add plumed.dat parser for plumed input file
    inclued read_ and write_ function.

--- src/kimmdy/parsing.py
+++ src/kimmdy/parsing.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from collections.abc import Iterable
 from typing import Generator
 
+from kimmdy.reaction import Topology
+
 
 def is_comment(l: str):
     return len(l) == 0 or l[0] in ["#", "\n", ";"]
@@ -23,7 +25,7 @@ def get_sections(
         yield data
 
 
-def read_topol(path: Path) -> dict[str, list[list[str]]]:
+def read_topol(path: Path) -> Topology:
     with open(path, "r") as f:
         sections = get_sections(f, "[")
         return {
@@ -32,10 +34,53 @@ def read_topol(path: Path) -> dict[str, list[list[str]]]:
         }
 
 
-def write_topol(d: dict[str, list[list[str]]], outfile: Path):
+def write_topol(d: Topology, outfile: Path):
     with open(outfile, "w") as f:
         for title, content in d.items():
             s = f"[ {title} ]\n"
             f.write(s)
             s = "\n".join([" ".join(c) for c in content]) + "\n\n"
             f.write(s)
+
+
+def read_plumed(path: Path):
+    with open(path, "r") as f:
+        distances = []
+        prints = []
+        for l in f:
+            if "DISTANCE" in l:
+                d = l.split()
+                distances.append(
+                    {
+                        "id": d[0].strip(":"),
+                        "keyword": d[1],
+                        "atoms": d[2].strip("ATOMS=").split(","),
+                    }
+                )
+            if "PRINT" in l[:5]:
+                l = l.split()
+                d = {
+                    "PRINT": l[0]
+                }
+                for x in l[1:]:
+                    key, value = x.split("=")
+                    d[key] = value
+
+                # TODO this could be better. the keys may not exist
+                # and break the program here.
+                d["ARG"] = d["ARG"].split(",")
+                d["STRIDE"] = int(d["STRIDE"])
+                d["FILE"] = Path(d["FILE"])
+
+                prints.append(d)
+
+        return {"distances": distances, "prints": prints}
+
+
+def write_plumed(d, path: Path):
+    with open(path, "w") as f:
+        for l in d["distances"]:
+            f.write(f"{l['id']}: {l['keyword']} ATOMS={','.join(l['atoms'])}\n")
+        for l in d["prints"]:
+            f.write(f"{l['PRINT']} ARG={','.join(l['ARG'])} STRIDE={str(l['STRIDE'])} FILE={str(l['FILE'])}\n")
+
