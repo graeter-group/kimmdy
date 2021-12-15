b85a3989b3b89cdb96c19eba4672e305704805a4
Jannik Buhr <jannik.m.buhr@gmail.com>
2021-12-15 13:37:40 +0100

    turn changemanger into a module instead of a class
    because it has (and should not have) and state of its own.

--- src/kimmdy/changemanager.py
+++ src/kimmdy/changemanager.py
@@ -1,62 +1,80 @@
 import logging
-from kimmdy.reaction import ConversionRecipe
+from kimmdy.reaction import ConversionRecipe, ConversionType, Topology
 from kimmdy.parsing import read_topol, write_topol
+from pathlib import Path
 
 
-class ChangeManager:
-    def __init__(self, recipe : ConversionRecipe):
-        self.breakpair = recipe.atom_idx
+def modify_top(recipe: ConversionRecipe, oldtop: Path, newtop: Path):
+    logging.info(f"Reading: {oldtop} and writing modified topology to {newtop}.")
+    topology = read_topol(oldtop)
 
-    def modify_top(self, oldtop, newtop):
-        logging.info(f"Reading: {oldtop} and writing modified topology to {newtop}.")
-        topology = read_topol(oldtop)
+    if recipe.type == ConversionType.BREAK:
+        topology = break_bond_top(topology, recipe.atom_idx)
+    elif recipe.type == ConversionType.MOVE:
+        raise NotImplementedError(
+            "Topology Changer for moving Atoms is not implemented yet."
+        )
 
-        # remove bond, angles and dihedrals where breakpair was involved
-        topology["bonds"] = [
-            bond for bond in topology["bonds"]
-            if not bond[0] in self.breakpair and not bond[1] in self.breakpair
-            ]
-        topology["pairs"] = [
-            pair for pair in topology["pairs"]
-            if not pair[0] in self.breakpair and not pair[1] in self.breakpair
-            ]
-        topology["angles"] = [
-            angle for angle in topology["angles"]
-            if not angle[0] in self.breakpair and not angle[1] in self.breakpair
-            ]
-        topology["dihedrals"] = [
-            dihedral for dihedral in topology["dihedrals"]
-            if not dihedral[1] in self.breakpair and not dihedral[2] in self.breakpair
-            ]
-
-        write_topol(topology, newtop)
+    write_topol(topology, newtop)
 
 
-    def modify_plumed(self, oldplumeddat, newplumeddat, newplumeddist):  # essential
-        broken_distnbr = []
+def break_bond_top(topology: Topology, breakpair: tuple[int, int]):
+    # remove bond, angles and dihedrals where breakpair was involved
+    topology["bonds"] = [
+        bond
+        for bond in topology["bonds"]
+        if not bond[0] in breakpair and not bond[1] in breakpair
+    ]
+    topology["pairs"] = [
+        pair
+        for pair in topology["pairs"]
+        if not pair[0] in breakpair and not pair[1] in breakpair
+    ]
+    topology["angles"] = [
+        angle
+        for angle in topology["angles"]
+        if not angle[0] in breakpair and not angle[1] in breakpair
+    ]
+    topology["dihedrals"] = [
+        dihedral
+        for dihedral in topology["dihedrals"]
+        if not dihedral[1] in breakpair and not dihedral[2] in breakpair
+    ]
+    return topology
 
-        file = open(newplumeddat, "w")  # open in  append mode
-        with open(oldplumeddat) as f:
-            for line in f:
-                if "ATOMS=" in line:
-                    split1 = line.split(",")
-                    atom2 = split1[-1][:-2]
-                    split2 = split1[0].split("=")
-                    atom1 = split2[-1]
-                    split3 = split2[0].split(":")
-                    dist_nbr = split3[0]
-                    if [atom1, atom2] in self.breakpair:
-                        line = "# --already broken-- " + line
-                        broken_distnbr.append(dist_nbr)
-                if "PRINT" in line:
-                    line.find(dist_nbr)
-                    for dist_nbr in broken_distnbr:
-                        line = line.replace(dist_nbr + ",", "")
-                    split = line.split()
-                    file_old = split[-1]
-                    line = line.replace(file_old, "FILE=" + str(newplumeddist))
 
-                file.write(line)
-        file.close()
-        return newplumeddat, newplumeddist
+def move_ebond_top(topology: Topology, movepair: tuple[int, int]):
+    pass
 
+
+def modify_plumed(
+    recipe: ConversionRecipe, oldplumeddat: Path, newplumeddat: Path, newplumeddist: Path
+):  
+    broken_distnbr = []
+
+    breakpair = recipe.atom_idx
+
+    file = open(newplumeddat, "w")  # open in  append mode
+    with open(oldplumeddat) as f:
+        for line in f:
+            if "ATOMS=" in line:
+                split1 = line.split(",")
+                atom2 = split1[-1][:-2]
+                split2 = split1[0].split("=")
+                atom1 = split2[-1]
+                split3 = split2[0].split(":")
+                dist_nbr = split3[0]
+                if [atom1, atom2] in breakpair:
+                    line = "# --already broken-- " + line
+                    broken_distnbr.append(dist_nbr)
+            if "PRINT" in line:
+                line.find(dist_nbr)
+                for dist_nbr in broken_distnbr:
+                    line = line.replace(dist_nbr + ",", "")
+                split = line.split()
+                file_old = split[-1]
+                line = line.replace(file_old, "FILE=" + str(newplumeddist))
+
+            file.write(line)
+    file.close()
+    return newplumeddat, newplumeddist
