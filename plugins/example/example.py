504dc741d25346a42adfe101df78a6973f1f815a
KRiedmiller <kai.riedmiller@web.de>
2022-03-10 11:31:45 +0100

    Implement plugin system for reactions.

---
+++ plugins/example/example.py
@@ -0,0 +1,17 @@
+# %%
+import sys
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
+
+#%%
+discovered_plugins = entry_points(group="kimmdy.plugins")
+discovered_plugins, "dummyreaction" in discovered_plugins.names
+#%%
+reaction = discovered_plugins["dummyreaction"].load()
+r = reaction()
+type(r)
+
+#%%
