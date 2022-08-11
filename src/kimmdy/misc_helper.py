dafa5a0927ebf7be7ed0c6f2b959c178d87f6bc3
KRiedmiller <kai.riedmiller@web.de>
2022-08-11 10:24:29 +0200

    Handle shell expansions

--- src/kimmdy/misc_helper.py
+++ src/kimmdy/misc_helper.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import subprocess
+from typing import Union
 
 
-def concat_traj(run_dir: Path, out: Path, run_types=None):
+def concat_traj(run_dir: Union[Path, str], out: Union[Path, str], run_types=None):
     """Find and concatenate trajectories from KIMMDY runs.
 
     Parameters
@@ -11,10 +12,15 @@ def concat_traj(run_dir: Path, out: Path, run_types=None):
         Directory containing directories of multiple tasks.
     out : Path
         File Path into the output trr will be written.
+        Can be relative to run_dir
     run_types : list, optional
         List of tasks to get trrs from. If a task is not in this list
         it will be skipped. By default None
     """
+    run_dir = Path(run_dir).expanduser().resolve()
+    out = Path(out).expanduser()
+    assert out.suffix == ".trr", "Output file should be a trr file."
+
     dirs = sorted(
         list(filter(lambda d: d.is_dir(), run_dir.iterdir())),
         key=lambda p: int(p.name.split("_")[0]),
@@ -28,7 +34,6 @@ def concat_traj(run_dir: Path, out: Path, run_types=None):
         trrs.extend(d.glob("*.trr"))
     trrs = [str(t) for t in trrs]
 
-    assert out.suffix == ".trr", "Output file should be a trr file."
     command = f"gmx trjcat -f {' '.join(trrs)} -o {str(out)} -cat".split(" ")
 
     subprocess.run(command, cwd=run_dir)
