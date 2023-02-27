8d55670d93b537405de675c6b7a47d0216f967e5
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-02-27 14:16:03 +0100

    debug: add debug document

--- example/example_ala/cat-trj.sh
+++ example/example_ala/cat-trj.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 
-dir='004'
-
-gmx trjcat -cat -o cat.trr  -f \
-  hat_tf_${dir}/1_equilibration/equil.trr\
-  hat_tf_${dir}/2_equilibration/equil.trr\
-  hat_tf_${dir}/5_relaxation/relax.trr\
-  hat_tf_${dir}/6_equilibration/equil.trr\
-  hat_tf_${dir}/9_relaxation/relax.trr\
-  hat_tf_${dir}/10_equilibration/equil.trr\
-  hat_tf_${dir}/13_relaxation/relax.trr\
-  hat_tf_${dir}/14_equilibration/equil.trr\
-  hat_tf_${dir}/17_relaxation/relax.trr\
-  hat_tf_${dir}/18_equilibration/equil.trr\
-  hat_tf_${dir}/21_relaxation/relax.trr\
-  hat_tf_${dir}/22_equilibration/equil.trr\
-  hat_tf_${dir}/25_relaxation/relax.trr\
-  hat_tf_${dir}/26_equilibration/equil.trr\
-  hat_tf_${dir}/29_relaxation/relax.trr\
-  hat_tf_${dir}/30_equilibration/equil.trr\
-  hat_tf_${dir}/33_relaxation/relax.trr\
-  hat_tf_${dir}/34_equilibration/equil.trr\
-  hat_tf_${dir}/37_relaxation/relax.trr\
-  hat_tf_${dir}/38_equilibration/equil.trr\
-  hat_tf_${dir}/41_relaxation/relax.trr
+for path in\
+  hat_tf_000/1_equilibration/equil\
+  hat_tf_000/2_equilibration/equil\
+  hat_tf_000/5_relaxation/relax\
+  hat_tf_000/6_equilibration/equil\
+  hat_tf_000/9_relaxation/relax; do
+echo "1 0" | gmx trjconv -f ${path}.trr -s ${path}.tpr -o ${path}-center.trr -center -pbc mol
+done
 
 
-echo "1 1" | gmx trjconv -dump 0 -f cat.trr -s ./hat_tf_000/1_equilibration/equil.tpr -center -pbc mol -o cat-center.gro
-echo "1 1" | gmx trjconv -f cat.trr -s ./hat_tf_000/1_equilibration/equil.tpr -center -pbc mol -o cat-center.xtc
+gmx trjcat -cat -o cat-center.xtc -f \
+  hat_tf_000/1_equilibration/equil-center.trr\
+  hat_tf_000/2_equilibration/equil-center.trr\
+  hat_tf_000/5_relaxation/relax-center.trr\
+  hat_tf_000/6_equilibration/equil-center.trr\
+  hat_tf_000/9_relaxation/relax-center.trr
 
+echo "1 0" | gmx trjconv -dump 0 -f cat.trr -s ./hat_tf_000/1_equilibration/equil.tpr -center -pbc mol -o cat-center.gro
 
