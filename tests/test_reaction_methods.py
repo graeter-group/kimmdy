79baa08022f3dfdef77089993a8d91f28e2026eb
KRiedmiller <kai.riedmiller@web.de>
2023-05-31 09:09:38 +0200

    refactor: rm avg rate/time -> timespans

--- tests/test_reaction_methods.py
+++ tests/test_reaction_methods.py
@@ -4,18 +4,13 @@ import pytest
 from dataclasses import asdict
 
 
-def test_no_double_times():
-    with pytest.raises(ValueError):
-        Recipe([RecipeStep()], rates=[1.0, 2.0, 3.0], times=[1.0, 1.0, 2.0])
-
-
 def test_combine_recipes():
-    rp1a = Recipe([RecipeStep()], rates=[1], times=[1.0])
-    rp1b = Recipe([RecipeStep()], rates=[1], times=[2.0])
-    rp2 = Recipe([RecipeStep(), RecipeStep()], rates=[1], times=[3.0])
+    rp1a = Recipe([RecipeStep()], rates=[1], timespans=[[0.0, 1.0]])
+    rp1b = Recipe([RecipeStep()], rates=[1], timespans=[[1.0, 2.0]])
+    rp2 = Recipe([RecipeStep(), RecipeStep()], rates=[1], timespans=[[1.0,3.0]])
 
     rp1a.combine_with(rp1b)
-    assert rp1a.times == [1, 2]
+    assert rp1a.timespans == [[0.0, 1.0], [1.0, 2.0]]
     assert rp1a.rates == [1, 1]
 
     with pytest.raises(ValueError):
@@ -25,12 +20,12 @@ def test_combine_recipes():
 @pytest.fixture
 def recipe_collection():
     rps = [
-        Recipe([RecipeStep(), RecipeStep(), RecipeStep()], rates=[1], times=[0.0]),
-        Recipe([RecipeStep()], rates=[1], times=[1.0]),
-        Recipe([RecipeStep()], rates=[1], times=[2.0]),
-        Recipe([RecipeStep(), RecipeStep()], rates=[1], times=[3.0]),
-        Recipe([RecipeStep()], rates=[1], times=[4.0]),
-        Recipe([RecipeStep(), RecipeStep()], rates=[1], times=[5.0]),
+        Recipe([RecipeStep(), RecipeStep(), RecipeStep()], rates=[1], timespans=[[0.0, 1.0]]),
+        Recipe([RecipeStep()], rates=[1], timespans=[[0.0, 1.0]]),
+        Recipe([RecipeStep()], rates=[1], timespans=[[1.0, 2.0]]),
+        Recipe([RecipeStep(), RecipeStep()], rates=[1], timespans=[[2.0, 3.0]]),
+        Recipe([RecipeStep()], rates=[1], timespans=[[3.0, 4.0]]),
+        Recipe([RecipeStep(), RecipeStep()], rates=[1], timespans=[[4.0, 5.0]]),
     ]
     return RecipeCollection(rps)
 
@@ -39,8 +34,8 @@ def test_aggregate_recipe_collection(recipe_collection):
     recipe_collection.aggregate_reactions()
 
     assert len(recipe_collection.recipes) == 3
-    assert recipe_collection.recipes[1].times == [1.0, 2.0, 4.0]
-    assert recipe_collection.recipes[2].times == [3.0, 5.0]
+    assert recipe_collection.recipes[1].timespans == [[0.0, 1.0], [1.0, 2.0], [3.0, 4.0]]
+    assert recipe_collection.recipes[2].timespans == [[2.0, 3.0], [4.0, 5.0]]
 
 
 def test_recipe_collection_to_csv(tmp_path, recipe_collection):
@@ -51,7 +46,7 @@ def test_recipe_collection_to_csv(tmp_path, recipe_collection):
         rows = [r for r in reader]
     for read_rp, org_rp in zip(rows, recipe_collection.recipes):
         org_rp_d = asdict(org_rp)
-        keys_to_check = ["rates", "times", "avg_rates", "avg_timespans"]
+        keys_to_check = ["rates", "timespans"]
         for key in keys_to_check:
             org_val = str(org_rp_d[key])
             if org_val == "None":
