c95d7c3fbbe201f56ead3d7cce84417b84403a09
KRiedmiller <kai.riedmiller@web.de>
2023-06-02 15:56:40 +0200

    feat: change coordinates in trr and gro

--- src/kimmdy/changemanager.py
+++ src/kimmdy/changemanager.py
@@ -1,20 +1,89 @@
 from __future__ import annotations
 import logging
+import MDAnalysis as mda
 from typing import Optional
-from kimmdy.reaction import Recipe, Bind, Break, Move, RecipeStep
+import numpy as np
+from kimmdy.reaction import Bind, Break, Move, RecipeStep
 from kimmdy.parsing import read_topol, write_topol, write_plumed, read_plumed
+from kimmdy.tasks import TaskFiles
 from kimmdy.topology.topology import Topology
 from pathlib import Path
 
 
+def modify_coords(
+    recipe_steps: list[RecipeStep],
+    files: TaskFiles,
+):
+    logging.debug(f"Entering modify_coords with recipe_steps {recipe_steps}")
+
+    trr = files.input["trr"]
+    tpr = files.input["tpr"]
+    trr_out = files.outputdir / "coord_mod.trr"
+    gro_out = files.outputdir / "coord_mod.gro"
+    files.output["trr"] = trr_out
+    files.output["gro"] = gro_out
+
+    assert not trr_out.exists(), f"Error: Output trr exists! {trr_out}"
+    assert not gro_out.exists(), f"Error: Output gro exists! {gro_out}"
+
+    u = mda.Universe(str(tpr), str(trr), topology_format="tpr", format="trr")
+
+    ttime = None
+    to_move = []
+    for step in recipe_steps:
+        if isinstance(step, Move) and step.new_coords is not None:
+            assert not step.is_one_based, "RecipeSteps should be zero based!"
+            if ttime is None:
+                ttime = step.new_coords[1]
+
+            elif abs(ttime - step.new_coords[1]) > 1e-5:  # 0.01 fs
+                m = (
+                    f"Multiple coordinate changes requested at different times!"
+                    "\nRecipeSteps:{recipe_steps}"
+                )
+                logging.error(m)
+                raise ValueError(m)
+            to_move.append(step.idx_to_move)
+
+    np.unique(to_move, return_counts=True)
+
+    for ts in u.trajectory:
+        if abs(ts.time - ttime) > 1e-5:  # 0.01 fs
+            continue
+        for step in recipe_steps:
+            if isinstance(step, Move) and step.new_coords is not None:
+                atm_move = u.select_atoms(f"index {step.idx_to_move}")
+                atm_move.position = step.new_coords[0]
+
+        break
+    else:
+        raise LookupError(
+            f"Did not find time {ttime} in trajectory "
+            f"with length {u.trajectory[-1].time}"
+        )
+
+    u.atoms.write(trr_out)
+    u.atoms.write(gro_out)
+
+    files.output = {"trr": trr_out}
+    files.output = {"gro": gro_out}
+
+    logging.debug(
+        f"Exit modify_coords, final coordinates written to {trr_out.parts[-2:]}"
+    )
+
+
 def modify_top(
     recipe_steps: list[RecipeStep],
-    oldtop: Path,
-    newtop: Path,
-    ffdir: Path,
+    files: TaskFiles,
     ffpatch: Optional[Path],
     topology: Optional[Topology],
-) -> Topology:
+):
+    files.output = {"top": files.outputdir / "topol_mod.top"}
+    oldtop = (files.input["top"],)
+    newtop = (files.output["top"],)
+    ffdir = (files.input["ff"],)
+
     logging.info(f"Reading: {oldtop} and writing modified topology to {newtop}.")
     if topology is None:
         topologyDict = read_topol(oldtop)
@@ -47,8 +116,6 @@ def modify_top(
                 topology.break_bond([step.idx_to_move, step.idx_to_break])
                 focus.add(str(step.idx_to_move))
                 focus.add(str(step.idx_to_break))
-            if step.new_coords is not None:
-                raise NotImplementedError("Changing coordinates not implemented!")
 
         else:
             raise NotImplementedError(f"RecipeStep {step} not implemented!")
@@ -57,7 +124,7 @@ def modify_top(
 
     topology.patch_parameters(list(focus))
 
-    return topology
+    return
 
 
 def modify_plumed(
