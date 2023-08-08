1d22131fadae56bb0bbb45fd6b039264e6f1340a
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-08-08 11:03:25 +0200

    docs(types): add custom renderer
    now auto-links type annotations.
    Can be removed once the feature is upstreamed in
    quartodoc
    
    refactor(naming): change `iterations` to `max_tasks`
    
    and add useful description for it.
    
    chore(docs): render

--- src/kimmdy/changemanager.py
+++ src/kimmdy/changemanager.py
@@ -41,7 +41,7 @@ def modify_coords(
     Parameters
     ----------
     recipe_steps :
-        A list of [](`~kimmdy.tasks.Task`) [](`~kimmdy.reaction.RecipeStep`) where each steps contains a `new_coords`
+        A list of [](`~kimmdy.reaction.RecipeStep`) where each steps contains a `new_coords`
         parameter.
     files :
         Input and output files for this [](`~kimmdy.tasks.Task`).
@@ -145,10 +145,10 @@ def modify_top(
     Parameters
     ----------
     recipe_steps :
-        A list of [RecipeStep](``kimmdy.reaction.RecipeStep``)s.
+        A list of [](`~kimmdy.reaction.RecipeStep`)s.
         parameter.
     files :
-        Input and output files for this [Task](``kimmdy.tasks.Task``).
+        Input and output files for this [](`~kimmdy.tasks.Task`).
         files.input:
             - top
         files.output:
@@ -215,7 +215,7 @@ def modify_plumed(
     Parameters
     ----------
     recipe_steps :
-        A list of [RecipeStep](`~kimmdy.reaction.RecipeStep`)s.
+        A list of [](`~kimmdy.reaction.RecipeStep`)s.
         parameter.
     """
     logging.info(
