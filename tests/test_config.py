f0be0560f57d47b0d6f54dfadd17a0dcdace64ab
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-09-11 11:20:32 +0200

    more tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,9 +7,6 @@ import pytest
 @pytest.mark.require_gmx
 def test_parse_config1_casting(arranged_tmp_path):
     input_f = Path("config1.yml")
-
-    assert input_f.exists(), "Input file not found"
-
     config = Config(input_f)
 
     assert config.dryrun is True
@@ -25,11 +22,64 @@ def test_parse_config1_casting(arranged_tmp_path):
     assert isinstance(config.mds.pull1.use_plumed, bool)
 
 
+def test_non_existent_sections_with_defaults_in_subsections_are_created(arranged_tmp_path):
+    path = Path("config1.yml")
+    config = Config(path)
+    assert config.log.file.name == "kimmdy.log"
+    assert config.log.level == "INFO"
+
+
+def test_no_sections_are_created_for_not_mentioned_reactions(arranged_tmp_path):
+    path = Path("config1.yml")
+    config = Config(path)
+    assert config.reactions.homolysis
+    assert config.reactions.homolysis.edis.name == "edissoc.dat"
+    assert len(config.reactions.__dict__) == 1
+
+
+    path = Path("config7.yml")
+    config = Config(path)
+    assert len(config.reactions.__dict__) == 2
+    assert config.reactions.homolysis
+    assert config.reactions.homolysis.edis.name == "edissoc.dat"
+    assert config.reactions.hat_naive
+    assert config.reactions.hat_naive.polling_rate == 1
+    assert config.reactions.hat_naive.h_cutoff == 4
+
+def test_subsections_with_defaults_are_kept(arranged_tmp_path):
+    path = Path("config7.yml")
+    config = Config(path)
+    assert config.log.file.name == "kimmdy.log"
+    assert config.log.level == "DEBUG"
+
+def test_out_is_generated_from_name_if_not_set(arranged_tmp_path):
+    path = Path("config1.yml")
+    config = Config(path)
+    assert config.name == 'kimmdy'
+    assert config.out.name == 'test_config_1'
+
+    path = Path("config7.yml")
+    config = Config(path)
+    assert config.name == 'config7'
+    assert config.out.name == 'config7'
+
+def test_general_settings_for_mds_are_set(arranged_tmp_path):
+    path = Path("config1.yml")
+    config = Config(path)
+    assert len(config.mds.__dict__) == 3
+    # explicitly set:
+    assert config.mds.equilibrium1.mdp.name == 'pullf1500_equil.mdp'
+    assert config.mds.pull1.use_plumed == True
+    # '.*' defaults:
+    assert config.mds.equilibrium1.use_plumed == False
+
+def test_complains_if_plumed_used_but_not_set(arranged_tmp_path):
+    path = Path("config8.yml")
+    with pytest.raises(AssertionError, match='Plumed requested in md section, but not defined at config root'):
+        _ = Config(path)
+
 def test_parse_config2_start_with_reaction(arranged_tmp_path):
     input_f = Path("config2.yml")
-
-    assert input_f.exists(), "Input file not found"
-
     config = Config(input_f)
 
     assert isinstance(config.tpr, Path)
@@ -41,16 +91,12 @@ def test_parse_config2_start_with_reaction(arranged_tmp_path):
 def test_parse_config3_missing_mdp_file(arranged_tmp_path):
     input_f = Path("config3.yml")
 
-    assert input_f.exists(), "Input file not found"
-
     with pytest.raises(LookupError):
         Config(input_f)
 
 
 def test_parse_config4_sequence_missing_entry(arranged_tmp_path):
     input_f = Path("config4.yml")
-
-    assert input_f.exists(), "Input file not found"
 
     with pytest.raises(AssertionError):
         Config(input_f)
@@ -59,8 +105,6 @@ def test_parse_config4_sequence_missing_entry(arranged_tmp_path):
 def test_parse_config5_sequence_missing_entry_no_mds(arranged_tmp_path):
     input_f = Path("config5.yml")
 
-    assert input_f.exists(), "Input file not found"
-
     with pytest.raises(AssertionError):
         Config(input_f)
 
@@ -68,17 +112,12 @@ def test_parse_config5_sequence_missing_entry_no_mds(arranged_tmp_path):
 def test_parse_config6_changer_bad_reference(arranged_tmp_path):
     input_f = Path("config6.yml")
 
-    assert input_f.exists(), "Input file not found"
-
     with pytest.raises(AssertionError):
         Config(input_f)
 
 
 def test_get_existing_files(arranged_tmp_path):
     input_f = Path("config1.yml")
-
-    assert input_f.exists(), "Input file not found"
-
     config = Config(input_f)
     file_d = get_existing_files(config)
     assert set(file_d.keys()) == set(
