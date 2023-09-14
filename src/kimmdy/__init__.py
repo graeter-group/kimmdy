1cb203092f1c0b40ce57c5dad1a00d88916935df
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-09-14 13:54:24 +0200

    fix: typing of plugins

--- src/kimmdy/__init__.py
+++ src/kimmdy/__init__.py
@@ -27,15 +27,15 @@ else:
         "kimmdy.parameterization_plugins"
     ]
 
-reaction_plugins: dict[str, ReactionPlugin] = {}
-broken_reaction_plugins: dict[str, Exception] = {}
+reaction_plugins: dict[str, type[ReactionPlugin]] = {}
+broken_reaction_plugins: dict[str,Exception] = {}
 for _ep in discovered_reaction_plugins:
     try:
         reaction_plugins[_ep.name] = _ep.load()
     except Exception as _e:
         broken_reaction_plugins[_ep.name] = _e
 
-parameterization_plugins: dict[str, Parameterizer] = {}
+parameterization_plugins: dict[str,  type[Parameterizer]] = {}
 broken_parameterization_plugins: dict[str, Exception] = {}
 for _ep in discovered_parameterization_plugins:
     try:
