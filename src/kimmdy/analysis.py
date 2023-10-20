77a8d726c223d45233c15100437d641c0c1cfc65
Jannik Buhr <17450586+jmbuhr@users.noreply.github.com>
2023-10-20 13:41:36 +0200

    feat: only plot radical occupied atoms (#308)
    feat: display residue name for radical population

--- src/kimmdy/analysis.py
+++ src/kimmdy/analysis.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib as mpl
 import seaborn.objects as so
+import seaborn as sns
 import argparse
 from seaborn import axes_style
 import pandas as pd
@@ -270,13 +271,8 @@ def radical_population(
     u = mda.Universe(str(gro[0]), format="gro")
     atoms = u.select_atoms(select_atoms)
     atoms_identifier = [
-        "-".join(x)
-        for x in list(
-            zip(
-                [str(resid) for resid in atoms.resids],
-                [str(name) for name in atoms.names],
-            )
-        )
+        "-".join(str(x) for x in [a.resid, a.resname, a.name])
+        for a in atoms
     ]
     atom_ids = atoms.ids
 
@@ -288,12 +284,16 @@ def radical_population(
             if int(idx) in counts.keys():
                 counts[int(idx)] += 1 / n_states
 
-    plt.bar(x=atoms_identifier, height=counts.values())
+    # filter out atoms with zero occupancy
+    occupied_counts = {atoms_identifier[k]: v for k, v in counts.items() if v > 0}
+
+    sns.barplot(x=list(occupied_counts.keys()),
+                    y=list(occupied_counts.values()),
+                    errorbar=None
+                    )
+
     plt.xlabel("Atom identifier")
     plt.ylabel("Fractional Radical Occupancy")
-    plt.ylim(0, 1)
-    plt.xticks(atoms_identifier, rotation=90, ha="right")
-    plt.tight_layout()
     output_path = str(analysis_dir / "radical_population_fingerprint.png")
     plt.savefig(output_path, dpi=300)
 
