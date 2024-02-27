e6b0c359ac9e46bf39f86786138f9eae87338f1c
ehhartmann <69237857+ehhartmann@users.noreply.github.com>
2024-02-27 13:15:57 +0100

    multipledihedrals for impropers (#387)
    * change topology object
    
    * fix top.to_dict() error for impropers
    
    * black
    
    * fix coordinates part
    
    ---------
    
    Co-authored-by: Eric Hartmann <hartmaec@rh05659.villa-bosch.de>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -47,6 +47,7 @@ def test_integration_valid_input_files(arranged_tmp_path):
 def test_grompp_with_kimmdy_topology(arranged_tmp_path):
     raw_top = read_top(Path("minimal.top"))
     top = Topology(raw_top)
+    top_dict = top.to_dict()
     write_top(top.to_dict(), Path("output.top"))
     assert sp.run(
         [
