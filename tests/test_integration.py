38bb63520ca02d0eee885075d82d6f207bfd0bfb
Eric Hartmann <hartmaec@rh05659.villa-bosch.de>
2024-03-06 17:11:49 +0100

    add test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,6 +7,7 @@ import pytest
 from kimmdy.cmd import kimmdy_run
 from kimmdy.parsing import read_top, write_top
 from kimmdy.topology.topology import Topology
+from kimmdy.plugins import parameterization_plugins
 
 
 def read_last_line(file):
@@ -63,6 +64,51 @@ def test_grompp_with_kimmdy_topology(arranged_tmp_path):
             "minial.tpr",
         ],
         check=True,
+    )
+
+
+@pytest.mark.require_grappa
+@pytest.mark.parametrize(
+    "arranged_tmp_path", (["test_integration/triplehelix_pull"]), indirect=True
+)
+def test_grappa_partial_parameterization(arranged_tmp_path):
+    parametrizer = parameterization_plugins["grappa"]()
+
+    # parameterize everything
+    top = Topology(read_top(Path("topol.top")), parametrizer=parametrizer)
+    top.needs_parameterization = True
+    top.update_parameters()
+
+    # parameterize around delete
+    top_del = Topology(read_top(Path("topol.top")), parametrizer=parametrizer)
+    top_del.needs_parameterization = True
+    top_del.update_parameters()
+    top_del.break_bond(("1", "5"))
+    top_del.needs_parameterization = True
+    top_del.update_parameters(focus_nrs=set(["1", "5"]))
+
+    # check values are changed around homolysis atoms
+    assert top.bonds[("5", "6")] != top_del.bonds[("5", "6")]
+    assert top.angles[("6", "5", "7")] != top_del.angles[("6", "5", "7")]
+    assert (
+        top.proper_dihedrals[("13", "11", "14", "15")]
+        != top_del.proper_dihedrals[("13", "11", "14", "15")]
+    )
+    assert (
+        top.improper_dihedrals[("8", "5", "7", "9")]
+        != top_del.improper_dihedrals[("8", "5", "7", "9")]
+    )
+
+    # check values are the same further away
+    assert top.bonds[("200", "203")] == top_del.bonds[("200", "203")]
+    assert top.angles[("228", "230", "232")] == top_del.angles[("228", "230", "232")]
+    assert (
+        top.proper_dihedrals[("1714", "1713", "1715", "1717")]
+        == top_del.proper_dihedrals[("1714", "1713", "1715", "1717")]
+    )
+    assert (
+        top.improper_dihedrals[("349", "357", "355", "356")]
+        == top_del.improper_dihedrals[("349", "357", "355", "356")]
     )
 
 
