de4c25b63c398f020a44c178053e666fdce01fbf
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-06-17 14:51:31 +0200

    report output directory before finishing

--- src/kimmdy/runmanager.py
+++ src/kimmdy/runmanager.py
@@ -223,6 +223,7 @@ class RunManager:
         logger.info(
             f"Finished running tasks, state: {self.state} after "
             f"{timedelta(seconds=(time.time() - self.start_time))}"
+            f"In output directory {self.config.out}"
         )
 
     def _setup_tasks(self):
