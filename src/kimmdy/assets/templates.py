4be3711c6f6691a398d3dca4e67de64ed2822aa5
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-08-28 12:47:33 +0200

    fix escape sequence in jobscript template string

--- src/kimmdy/assets/templates.py
+++ src/kimmdy/assets/templates.py
@@ -40,7 +40,7 @@ if [ $HOURS -lt $CYCLE ]; then
   exit 3
 else
   echo "jobscript resubmitting"
-  sed -i.bak "s/\(run_directory:\s*\).*/\\1'{config.out.name}'/" {config.input_file}
+  sed -i.bak "s/\\(run_directory:\\s*\\).*/\\1'{config.out.name}'/" {config.input_file}
   kimmdy --generate-jobscript
   sbatch ./jobscript.sh
   exit 2
