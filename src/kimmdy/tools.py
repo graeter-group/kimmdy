505c823afdd523f3c7170be270f06c8aa1fc18bf
KRiedmiller <kai.riedmiller@web.de>
2024-09-11 16:07:20 +0200

    feat: expose create_plumed_input (#483)
    * feat: make stride variable
    * feat: expose plumed input creation in cli
    * doc: update changes
    * make indexgroup interactive and fix file handling

--- src/kimmdy/tools.py
+++ src/kimmdy/tools.py
@@ -424,3 +424,134 @@ def write_top_as_dot(top: Topology, path: str, overlap: str = "true"):
     """
     with open(path, "w") as f:
         f.write(top_to_graph(top, overlap))
+
+
+def create_plumed_dat(
+    top_path: Path,
+    idx_path: Path,
+    idx_group: Optional[str],
+    entries_to_remove: list = ["C-N", "H*", "O*"],
+    stride: int = 100,
+):
+    print(
+        "Creating plumed file with following settings:\n\n"
+        f"top_path: \t\t{top_path}\n"
+        f"idx_path: \t\t{idx_path}\n"
+        f"idx_group: \t\t{idx_group}\n"
+        f"entries_to_remove: \t\t{entries_to_remove}\n"
+    )
+
+    # init variables
+    out_file = top_path.with_name("plumed.dat")
+    if out_file.exists():
+        print(f"Error: {out_file} does exists!")
+        return
+
+    top = Topology(read_top(top_path))
+    idx = read_top(idx_path)
+    plumed_distances_out = "distances.dat"
+
+    # prepare remove variables
+    atoms_to_remove = []
+    atoms_start_to_remove = []
+    bonds_to_remove = []
+    for entry in entries_to_remove:
+        entrysplit = entry.split(sep="-")
+        if len(entrysplit) == 1:
+            if entrysplit[0].endswith("*"):
+                atoms_start_to_remove.append(entrysplit[0][:-1])
+            else:
+                atoms_to_remove.append(entrysplit[0])
+        elif len(entrysplit) == 2:
+            bonds_to_remove.append(set(entrysplit))
+        else:
+            print(f"Don't know how to deal with entry {entry}, skipping it!")
+
+    if idx_group is None:
+        print("Which index group should be written to the plumed input?")
+        for g in idx.keys():
+            print(g)
+        print("")
+        while idx_group is None:
+            _idx_group = input("Please enter the index group:")
+            if _idx_group in idx.keys():
+                idx_group = _idx_group
+            else:
+                print(f"Error: {_idx_group} not in index file.")
+
+    # create set of indices from the specified index group
+    try:
+        idxs_parsed = idx[idx_group]["content"]
+    except KeyError as e:
+        raise KeyError(
+            f" Did not find index group '{idx_group}' in index file groups: {list(idx.keys())}"
+        ) from e
+
+    idxs_set = set([entry for l in idxs_parsed for entry in l])
+
+    # find bonds with both indices in the index group
+    plumed_distance_idxs = []
+    for bond in top.bonds.keys():
+        if bond[0] in idxs_set and bond[1] in idxs_set:
+            ai_name = top.atoms[bond[0]].atom
+            aj_name = top.atoms[bond[1]].atom
+            if set([ai_name, aj_name]) in bonds_to_remove:
+                continue
+            if any([b in atoms_to_remove for b in [ai_name, aj_name]]):
+                continue
+            if any([b[0] in atoms_start_to_remove for b in [ai_name, aj_name]]):
+                continue
+            plumed_distance_idxs.append(bond)
+
+    # write plumed configuration file
+    print_arg = ""
+    with open(out_file, "w") as file:
+        file.write("#Define distances \n")
+        for i, dist in enumerate(plumed_distance_idxs):
+            file.write(f"d{i}: DISTANCE ATOMS={dist[0]},{dist[1]}\n")
+            print_arg += f"d{i},"
+        file.write(" \n#Print distances ARG to FILE every STRIDE steps \n")
+        file.write(
+            "PRINT ARG=" + print_arg + f" STRIDE={stride} FILE={plumed_distances_out}"
+        )
+
+
+def entry_point_create_plumed():
+    parser = argparse.ArgumentParser(
+        description="Build plumed input file. Requires a topology file and a gromacs index file."
+    )
+    parser.add_argument(
+        "--entries-to-remove",
+        nargs="*",
+        default=["C-N", "H*", "O*"],
+        help=(
+            "Either atomnames or bonds, i.e. atomnames separated by a hyphen "
+            "that should not be written to the plumed configuration file. Default: C-N H* O*"
+        ),
+    )
+    parser.add_argument(
+        "--stride",
+        type=int,
+        default=100,
+        help="Frequency of plumed output as multiple of the md timestep. Default: 100",
+    )
+    parser.add_argument(
+        "-p", "--top", type=str, required=True, help="Gromacs topology file path."
+    )
+    parser.add_argument(
+        "-i", "--index", type=str, required=True, help="Gromacs index file path."
+    )
+    parser.add_argument(
+        "--indexgroup",
+        type=str,
+        help="Index group name out of which bonds will be written to the plumed configuration file.",
+    )
+    p = parser.parse_args()
+
+    create_plumed_dat(
+        Path(p.top),
+        Path(p.index),
+        idx_group=p.indexgroup,
+        entries_to_remove=p.entries_to_remove,
+        stride=p.stride,
+    )
